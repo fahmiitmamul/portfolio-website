a52ccc415d2ede649766dda4c6c0e95e90cfb55b
fahmipraga <itmamul2004@gmail.com>
2023-08-03 00:08:54 +0700

    fix: fix some code in pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,73 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import { ConfigProvider } from "antd";
 import theme from "../theme/themeConfig";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaGithub } from "react-icons/fa";
+import Profile from "../public/fahmi.jpg";
+import { BsLinkedin } from "react-icons/bs";
+import Image from "next/image";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const HomePage = () => (
-  <ConfigProvider theme={theme}>
-    <div className="App">
-      <div className="flex justify-center items-center w-full h-[70px] bg-transparent fixed">
-        <nav>
-          <ul className="flex gap-10">
-            <li className="text-lg font-bold cursor-pointer hover:text-[#30E3CA]">
-              Home
-            </li>
-            <li className="text-lg font-bold cursor-pointer hover:text-[#30E3CA]">
-              About
-            </li>
-            <li className="text-lg font-bold cursor-pointer hover:text-[#30E3CA]">
-              Skills
-            </li>
-            <li className="text-lg font-bold cursor-pointer hover:text-[#30E3CA]">
-              Projects
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className="flex w-full h-[800px] body pt-[70px]">
-        <div className="text-4xl"></div>
-      </div>
-    </div>
-  </ConfigProvider>
-);
+export default function Homepage(): JSX.Element {
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
-export default HomePage;
+  return (
+    <ConfigProvider theme={theme}>
+      <div className="App">
+        <div className="flex justify-between items-center w-full h-[70px] bg-white px-10 text-[#383838]">
+          <div>
+            <p className="font-bold text-2xl uppercase tracking-widest">
+              portfolio
+            </p>
+          </div>
+          <nav className="flex justify-center items-center gap-10">
+            <ul className="flex gap-10">
+              <li className="text-lg font-bold cursor-pointer hover:text-orange-300">
+                About
+              </li>
+              <li className="text-lg font-bold cursor-pointer hover:text-orange-300">
+                Skills
+              </li>
+              <li className="text-lg font-bold cursor-pointer hover:text-orange-300">
+                Projects
+              </li>
+            </ul>
+            <div className="flex justify-center items-center gap-5">
+              <AiOutlineInstagram size={25} />
+              <BsLinkedin size={20} />
+              <FaGithub size={25} />
+            </div>
+          </nav>
+        </div>
+        <div className="flex w-full h-screen bg-gradient-to-tr from-blue-300 via-white to-orange-300">
+          <div
+            data-aos="fade-right"
+            className="flex-1 flex flex-col text-[#383838] justify-center items-center"
+          >
+            <div className="flex flex-col max-w-xl text-5xl font-black">
+              <div>Hi 👋,</div>
+              <div>I&apos;m Itmamul Fahmi</div>
+              <div className="text-xl">
+                Software Engineer | Fullstack Web Developer{" "}
+              </div>
+            </div>
+          </div>
+          <div
+            data-aos="fade-left"
+            className="flex-1 flex justify-center items-center"
+          >
+            <Image
+              alt="fahmi"
+              src={Profile}
+              className="w-64 shadow-2xl rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </ConfigProvider>
+  );
+}
