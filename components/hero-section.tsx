5aab928cb479b78788666806d7dd9bbce2151f01
fahmiitmamul <itmamul2004@gmail.com>
2025-05-07 11:12:27 +0700

    fix: change logo size

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -95,7 +95,7 @@ export function HeroSection() {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, delay: 0.2 }}
         >
-          <div className="relative w-[300px] h-[300px] md:w-[300px] md:h-[300px]">
+          <div className="relative w-[230px] h-[230px] md:w-[300px] md:h-[300px]">
             <div className="absolute inset-0 rounded-full bg-gradient-to-r from-orange-500 via-pink-500 to-purple-600 animate-rotate-slow"></div>
             <div className="absolute inset-[3px] rounded-full bg-gray-950 flex items-center justify-center">
               <div className="rounded-full overflow-hidden w-[calc(100%-6px)] h-[calc(100%-6px)]">
